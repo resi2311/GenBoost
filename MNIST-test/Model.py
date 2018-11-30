48bade1925880a0d8eef0c46f898e456d03cc1c9
LAPTOP-JOSQTPM1\resi2 <resi2311@mail.ru>
2018-11-30 21:16:29 +0300

    Commit 2

--- MNIST-test/Model.py
+++ MNIST-test/Model.py
@@ -3,9 +3,10 @@ import numpy as np
 import json
 from mnistprob import mnist_net
 from genboost import genboost
+import time
+from ElephantSender import sendNotification
 
 mnist = tf.keras.datasets.mnist
-
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 x_test = x_test[:1000]
@@ -37,21 +38,39 @@ def eval_model(weights):
         weight_layer_three,
         weight_layer_four
     ])
-    return np.array([-1.*model.evaluate(x_test, y_test)[1]])
+    return np.array([-1.*model.evaluate(x_test, y_test,verbose=0 )[1]])
 
-with open('parameters.json') as json_data:
+with open('parameters_2.json') as json_data:
     params = json.load(json_data)
 
 MyProb = mnist_net(fit_func=eval_model,dim=407050,lb=-1.,rb=1.)
 gb = genboost(problem=MyProb)
 
 results = []
+times = []
+TotalTime_0 = time.time()
 
 for param in params:
+    t0 = time.time()
+    print("Star of test.")
     pop = gb.run(param)
     results.append(pop.champion_f)
+    print('Parameters: {}'.format(param))
+    print('Fitness: {}'.format(pop.champion_f))
+    t1 = time.time() - t0
+    times.append(t1)
+    print("Time for test:{}".format(t1))
+    print('-'*20)
+print("Total time for all tests:{}".format(time.time() - TotalTime_0))
 
-for param, fitness in zip(params,results):
+for param, fitness,TestTime in zip(params,results, times):
     print('Parameters: {}'.format(param))
     print('Fitness: {}'.format(fitness))
+    print("Time for test:{}".format(TestTime))
     print('-'*20)
+
+for param, fitness, TestTime in zip(params,results,times):
+    sendNotification('Parameters: {}'.format(param))
+    sendNotification('Fitness: {}'.format(fitness))
+    sendNotification("Time for test:{}".format(TestTime))
+    sendNotification('-'*20)
