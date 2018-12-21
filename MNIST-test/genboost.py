bf1dc00df07f1116f965211af23f435155ec6697
alxkorn <akorneyev10@yandex.ru>
2018-12-21 18:33:12 +0300

    New tests and logs

--- MNIST-test/genboost.py
+++ MNIST-test/genboost.py
@@ -7,11 +7,12 @@ class genboost:
     def run(self, params : dict):
         self.params = copy.copy(params)
         self.prob = pg.problem(self.problem)
-        self.algo = self.params.pop('algo')
+        self.algo_name = self.params.pop('algo')
         self.ind_num = self.params.pop('ind_num')
-        self.algo = pg.algorithm(pg.__dict__[self.algo](**self.params))
+        self.algo = pg.algorithm(pg.__dict__[self.algo_name](**self.params))
         self.algo.set_verbosity(1)
         self.pop = pg.population(self.prob, self.ind_num)
         self.pop = self.algo.evolve(self.pop)
+        self.log = self.algo.extract(pg.__dict__[self.algo_name]).get_log()
         return self.pop
 
--- MNIST-test/test.py
+++ MNIST-test/test.py
@@ -2,7 +2,7 @@ import time
 import os
 import copy
 import json
-
+import pickle
 def test(gb, params_list : [dict], save_res = True, **kwargs): 
     results = []
     TotalTime_0 = time.time()
@@ -12,12 +12,20 @@ def test(gb, params_list : [dict], save_res = True, **kwargs):
         t0 = time.time()
         print("Star of test #{}.".format(i+1))
         pop = gb.run(param)
+
+        #temporary
+        logfile = 'log_{}.bin'.format(i)
+        log_path = 'logs'
+        with open(os.path.join(log_path, logfile),'wb') as f:
+            pickle.dump(gb.log, f)
+        #temporary
         t1 = time.time() - t0
         res_dct = copy.copy(param)
         res_dct['champ_f'] = float(pop.champion_f[0])
         res_dct['time'] = t1
         results.append(res_dct)
         print('\nParameters: {}\nFitness: {}\nTime for test: {:.3f}'.format(param, pop.champion_f[0], t1 / 60))
+        print('Log file: log_{}.bin'.format(i))
         print('-'*20)
     print("Total time for all tests:{:.3f}".format(time.time() - TotalTime_0))
 
